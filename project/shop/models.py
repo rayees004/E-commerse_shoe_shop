b86e8b5c9e0437a12dfe6444c02c75fd933b2e41
rayees <mrc53445@gmail.com>
2025-07-01 11:10:45 +0530

    home page database new table added named as Category

---
+++ project/shop/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.SlugField(max_length=250,unique=True)
